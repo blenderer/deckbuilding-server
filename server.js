25b3d58bd78f6df5de9063d11b048334aeed8c8b
Eric Harrison <eric.h@rrison.io>
2015-06-30 08:07:47 -0400

    DRY code and client/server 'node net'

--- server.js
+++ server.js
@@ -1,3 +1,48 @@
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+
+
+
+
+
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
+
+
+
+
+
+
+
+
+
 var Card = require('./classes/Card.js');
 var CardGroup = require('./classes/CardGroup.js');
 var CardStack = require('./classes/CardStack.js');
@@ -19,45 +64,34 @@ var dataCard2 = {
     image: null
 }
 
-var cards = [];
-
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard2));
-cards.push(new Card(dataCard2));
-cards.push(new Card(dataCard2));
-
-var deck = new CardStack(cards);
+var decks = [];
 
 
-cards = [];
+// 3 decks, 1 for each player, one purchase deck
+for (var i=0; i<3; i++) {
+    var cards = [];
 
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard));
-cards.push(new Card(dataCard2));
-cards.push(new Card(dataCard2));
-cards.push(new Card(dataCard2));
+    // Add the starter cards to deck
+    for (var j=0; j<7; j++) {
+        cards.push(new Card(dataCard));
+    }
 
-var deck2 = new CardStack(cards);
+    // Add fodder cards to deck
+    for (var j=0; j<3; j++) {
+        cards.push(new Card(dataCard2));
+    }
 
-var purchaseDeck = new CardStack(cards);
+    decks.push(new CardStack(cards));
+}
+
+var purchaseDeck = decks[2];
+
+
 purchaseDeck.shuffle();
 
-var p1 = new Player('Eric Harrison', deck);
-var p2 = new Player('Watson', deck2);
+var p1 = new Player('Eric Harrison', decks[0]);
+var p2 = new Player('Watson', decks[1]);
 
-console.log(p1.deck.cards.length, purchaseDeck.cards.length);
 p1.deck.add(purchaseDeck.takeTopCard());
-console.log(p1.deck.cards.length, purchaseDeck.cards.length);
 
 
